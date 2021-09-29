c2493edcb89d1edd8424b53d190da0266fc943ec
Zhukov Roman <zhukov.roman@gmail.com>
2021-09-29 18:57:17 +0300

    Initial commit
    Signed-off-by: Zhukov Roman <zhukov.roman@gmail.com>

---
+++ mask_pan.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"github.com/theplant/luhn"
+	"regexp"
+	"sort"
+	"strconv"
+)
+
+var digitReg = regexp.MustCompile("[0-9]")
+
+// returns map of integers positions (position:integer) in string
+func createIntMap(input string) map[int]int {
+	result := make(map[int]int)
+	for pos, char := range input {
+		if digitReg.MatchString(string(char)) {
+			num, _ := strconv.Atoi(string(char))
+			result[pos] = num
+		}
+	}
+
+	return result
+}
+
+// returns positions of PAN number in position mapped integers
+func findPanInIntMap(m map[int]int, from int) []int {
+	var result []int
+
+	keys := make([]int, len(m))
+
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+
+	if from > len(keys) {
+		return result
+	}
+
+	sort.Ints(keys)
+	keys = keys[from:]
+	keysLen := len(keys)
+
+	// min PAN number length
+	if keysLen < 16 {
+		return result
+	}
+
+OUTER:
+	for i = 0; i < keysLen-15; i++ {
+		var valueToCheck string
+		for ii := 0; ii < (keysLen - i); ii++ {
+			valueToCheck += fmt.Sprintf("%d", m[keys[i+ii]])
+			if valueToCheck[0] == '0' {
+				continue OUTER
+			}
+			num, err := strconv.Atoi(valueToCheck)
+			if len(valueToCheck) < 16 {
+				continue
+			}
+			if err != nil {
+				continue OUTER
+			}
+			if luhn.Valid(num) {
+				return keys[i : i+ii+1]
+			}
+		}
+	}
+
+	return result
+}
+
+func maskWithPositions(input string, pos []int) string {
+	sort.Ints(pos)
+	for _, p := range pos {
+		input = input[:p] + "*" + input[p+1:]
+	}
+
+	return input
+}
+
+// MaskPan returns string with masked PANs (Primary Account Number) in it
+// Pan detection based on Luhn algorithm (check wiki)
+func MaskPan(input string) string {
+	// integers map
+	intMap := createIntMap(input)
+	result := input
+	found := true
+	panPositions := findPanInIntMap(intMap, 0)
+	for found {
+		if panPositions != nil {
+			result = maskWithPositions(result, panPositions[4:len(panPositions)-4])
+			panPositions = findPanInIntMap(intMap, panPositions[len(panPositions)-1])
+		} else {
+			found = false
+		}
+	}
+	return result
+}
---
+++ mask_pan_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+)
+import "github.com/stretchr/testify/assert"
+
+func TestCreateIntMap(t *testing.T) {
+	actual := createIntMap("12 4d6")
+	expect := map[int]int{
+		0: 1,
+		1: 2,
+		3: 4,
+		5: 6,
+	}
+	assert.Equal(t, expect, actual)
+	assert.Equal(t, 0, len(createIntMap("abcd")))
+}
+
+func TestFindIntInMap(t *testing.T) {
+	assert.True(t, nil == findPanInIntMap(convertIntToMap(""), 0), "False positive result")
+	assert.True(t, nil == findPanInIntMap(convertIntToMap("0"), 0), "False positive result")
+	assert.Equal(
+		t,
+		[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+		findPanInIntMap(convertIntToMap("4000160000000004"), 0),
+		"Error in finding PAN (only PAN)",
+	)
+	assert.Equal(
+		t,
+		[]int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17},
+		findPanInIntMap(convertIntToMap("00400016000000000400"), 0),
+		"Error in finding PAN (PAN and surround zeroes)",
+	)
+}
+
+func TestMaskWithPositions(t *testing.T) {
+	assert.Equal(t, "****", maskWithPositions("1234", []int{0, 1, 2, 3}))
+	assert.Equal(t, "12**", maskWithPositions("1234", []int{2, 3}))
+	assert.Equal(t, "**34", maskWithPositions("1234", []int{0, 1}))
+}
+
+func TestMaskSinglePan(t *testing.T) {
+	assert.Equal(t, "4000********0004", MaskPan("4000160000000004"))
+	assert.Equal(t, "4000 **** **** 0004", MaskPan("4000 1600 0000 0004"))
+	assert.Equal(t, `{"pan":"4000 **** **** 0004"}`, MaskPan(`{"pan":"4000 1600 0000 0004"}`))
+	assert.Equal(t, `{"pan":"4000 1600 0000 0005"}`, MaskPan(`{"pan":"4000 1600 0000 0005"}`))
+}
+
+func TestMaskMultiplePans(t *testing.T) {
+	assert.Equal(t, `4000********0004 ♥ 4000********0004`, MaskPan(`4000160000000004 ♥ 4000160000000004`))
+	assert.Equal(t, "55 4000********0004 & 4000********0004", MaskPan("55 4000160000000004 & 4000160000000004"))
+	// TODO interesting false positive here - actual "40001600******** & *000160000000004"
+	// assert.Equal(t, "4000160000000005 & 4000********0004", MaskPan("4000160000000005 & 4000160000000004"))
+	// TODO and here - actual "4000**********05 & 4111111111111111"
+	// assert.Equal(t, "4000160000000005 & 4111********1111", MaskPan("4000160000000005 & 4111111111111111"))
+	assert.Equal(t, "4000160000000005 & 2223********0010", MaskPan("4000160000000005 & 2223000048410010"))
+	assert.Equal(t, "4000********0004 & 4000160000000005", MaskPan("4000160000000004 & 4000160000000005"))
+}
+
+func TestMaskMultipleDividedPans(t *testing.T) {
+	actual := `4000160000000004 ♥ 4000 ♥ 1600 0000 ♥ 0004`
+	expect := `4000********0004 ♥ 4000 ♥ **** **** ♥ 0004`
+	assert.Equal(t, expect, MaskPan(actual))
+}
+
+func TestMaskWithMultilineString(t *testing.T) {
+	actual := `400016
+0000000004`
+	expect := `4000**
+******0004`
+	assert.Equal(t, expect, MaskPan(actual))
+}
+
+func TestLongPan(t *testing.T) {
+	assert.Equal(t, "6771***********0006", MaskPan("6771830000000000006"))
+}
+
+func BenchmarkMask500(b *testing.B) {
+	symbols := "NYvMT1DUoalXFfOkBy0ZpqZ5rLKCOV2w1MjlaxaK9x9pVtkncLAarWCBm4mye8i3iIphrI1xnTjzINAPQ7fBuQMvAXGnZaJEYLnB7" +
+		"nJPATcbyvK37qLgNBfuiSnRSsc23s0hvSfB2doczZegkOPml4vOLQVFb7K310SGBIrhaV7YogaCH1N8g1punNWylBetnR0gYrALkpkTB38zX" +
+		"ksNRxSTjSucgVjD5e7QNGAHKCuCikvIuJeejKQe5DFFMFDKlP62zcR2pW5U2OghmBGAFkILXTySgCIr3v9jSLfuFeKcMfbDM7EbbJRBPcLNu" +
+		"RYngi9wFzdICfhnXBS6uYCdPzP69JEzBZAHLqLMOXyydlOKvNVVlA6pNAi34rcnJzR2hpFrkDIleuI0BrzlyVo7dj3SSuLj9W3brzQFfJTKQ" +
+		"kWbEsP9xl3cD7NdfdN4drJrqQu40001600000000045ENjdmT42VmDRrHzYota7YT98WwB6KVNZ"
+	for i := 0; i < b.N; i++ {
+		MaskPan(symbols)
+	}
+}
+
+func convertIntToMap(input string) map[int]int {
+	var r = make(map[int]int)
+	for pos, char := range input {
+		num, _ := strconv.Atoi(string(char))
+		r[pos] = num
+	}
+	return r
+}
